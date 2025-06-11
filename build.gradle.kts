0d483be96fc4365a31bac17ef3b5653b0e55027d
yuxuanchiadm <yuxuanchiadm@126.com>
2025-06-11 15:36:41 +0800

    Update build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 extra.set("copyTo", "{server}/plugins")
 
 dependencies {
-    compileOnly("cn.nukkit:nukkit")
+    compileOnly(project(":Nukkit"))
     compileOnly(project(":SynapseAPI"))
 }
 
